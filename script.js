b42b61a973ef5a155de87e8702ae8c4b664c44ad
Pedro Ramos <opedroramosbr@gmail.com>
2022-06-26 20:58:38 -0300

    opa

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 setTimeout(
     function tempo() {
         var elemento = document.getElementById('botao')
-        elemento.style.display = "block";
-
+        var elemento2 = document.getElementById('titlebbpai')
+        var elemento3 = document.getElementById('beneficios')
+        var elemento4 = document.getElementById('logodes')
+        var elemento5 = document.getElementById('titleddpai')
+        var elemento6 = document.getElementById('descobrir')
+        var elemento7 = document.getElementById('pitch')
+        var elemento8 = document.getElementById('botaopai2')
+        var elemento9 = document.getElementById('garantia')
+        var elemento10 = document.getElementById('perguntas')
+        elemento.style.display = "flex";
+        elemento2.style.display = "block";
+        elemento3.style.display = "flex";
+        elemento4.style.display = "block";
+        elemento5.style.display = "block";
+        elemento6.style.display = "flex";
+        elemento7.style.display = "flex";
+        elemento8.style.display = "flex";
+        elemento9.style.display = "block";
+        elemento10.style.display = "flex";
     }, /*tempo em milisegundos*/ 5000)
 /*var altura = window.screen.height
 console.log(altura)
