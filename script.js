2fbe7c3e0cd86aacad121e8814e283170040ea0e
Pedro Ramos <opedroramosbr@gmail.com>
2022-07-08 19:31:32 -0300

    opa

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 setTimeout(
     function tempo() {
-        var elemento = document.getElementById('botaopai')
+        var elemento = document.querySelector('#botaopai')
         var elemento2 = document.getElementById('titlebbpai')
         var elemento3 = document.getElementById('beneficios')
         var elemento4 = document.getElementById('logodes')
         var elemento5 = document.getElementById('titleddpai')
         var elemento6 = document.getElementById('descobrir')
         var elemento7 = document.getElementById('pitch')
-        var elemento8 = document.getElementById('botaopai2')
         var elemento9 = document.getElementById('garantia')
         var elemento10 = document.getElementById('perguntas')
-        var elemento11 = document.getElementById('pagamentos')
+        var elemento11 = document.querySelector('#pagamentos')
         var elemento12 = document.getElementById('pitch2')
-        var elemento13 = document.getElementById('vagas')
+        var elemento13 = document.querySelector('#vagas')
+        var elemento14 = document.querySelector('#botaopai2')
+        var elemento15 = document.querySelector('#pagamentos2')
+        var elemento16 = document.querySelector('#vagas2')
+
         elemento.style.display = "flex";
         elemento2.style.display = "block";
         elemento3.style.display = "flex";
@@ -20,13 +23,15 @@ setTimeout(
         elemento5.style.display = "block";
         elemento6.style.display = "flex";
         elemento7.style.display = "flex";
-        elemento8.style.display = "flex";
         elemento9.style.display = "block";
         elemento10.style.display = "flex";
         elemento11.style.display = "flex";
         elemento12.style.display = "flex";
         elemento13.style.display = "flex";
-    }, /*tempo em milisegundos*/ 5000)
+        elemento14.style.display = "flex";
+        elemento15.style.display = "flex";
+        elemento16.style.display = "flex";
+    }, /*tempo em milisegundos*/ 2000)
 /*var altura = window.screen.height
 console.log(altura)
 var largura = window.screen.width
